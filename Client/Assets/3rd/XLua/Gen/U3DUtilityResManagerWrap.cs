6ef3a5fb86ba7e8be64caeb8f74bea96b20a0b1f
RockerL <78175054@qq.com>
2018-04-09 13:32:59 +0800

    first commit

---
+++ Client/Assets/3rd/XLua/Gen/U3DUtilityResManagerWrap.cs
@@ -0,0 +1,216 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class U3DUtilityResManagerWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(U3DUtility.ResManager);
+			Utils.BeginObjectRegister(type, L, translator, 0, 4, 0, 0);
+			
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "GetLuaBytes", _m_GetLuaBytes);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "LoadAsset", _m_LoadAsset);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "CleanAllAsset", _m_CleanAllAsset);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "LoadAssetBundle", _m_LoadAssetBundle);
+			
+			
+			
+			
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 1, 1, 0);
+			
+			
+            
+			Utils.RegisterFunc(L, Utils.CLS_GETTER_IDX, "Singleton", _g_get_Singleton);
+            
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            
+			try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+				if(LuaAPI.lua_gettop(L) == 1)
+				{
+					
+					U3DUtility.ResManager __cl_gen_ret = new U3DUtility.ResManager();
+					translator.Push(L, __cl_gen_ret);
+                    
+					return 1;
+				}
+				
+			}
+			catch(System.Exception __gen_e) {
+				return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+			}
+            return LuaAPI.luaL_error(L, "invalid arguments to U3DUtility.ResManager constructor!");
+            
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_GetLuaBytes(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                U3DUtility.ResManager __cl_gen_to_be_invoked = (U3DUtility.ResManager)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    string name = LuaAPI.lua_tostring(L, 2);
+                    
+                        byte[] __cl_gen_ret = __cl_gen_to_be_invoked.GetLuaBytes( name );
+                        LuaAPI.lua_pushstring(L, __cl_gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_LoadAsset(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                U3DUtility.ResManager __cl_gen_to_be_invoked = (U3DUtility.ResManager)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    string assetPath = LuaAPI.lua_tostring(L, 2);
+                    System.Type type = (System.Type)translator.GetObject(L, 3, typeof(System.Type));
+                    
+                        UnityEngine.Object __cl_gen_ret = __cl_gen_to_be_invoked.LoadAsset( assetPath, type );
+                        translator.Push(L, __cl_gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_CleanAllAsset(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                U3DUtility.ResManager __cl_gen_to_be_invoked = (U3DUtility.ResManager)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    
+                    __cl_gen_to_be_invoked.CleanAllAsset(  );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_LoadAssetBundle(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                U3DUtility.ResManager __cl_gen_to_be_invoked = (U3DUtility.ResManager)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    string assetBundleName = LuaAPI.lua_tostring(L, 2);
+                    
+                        UnityEngine.AssetBundle __cl_gen_ret = __cl_gen_to_be_invoked.LoadAssetBundle( assetBundleName );
+                        translator.Push(L, __cl_gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_Singleton(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			    translator.Push(L, U3DUtility.ResManager.Singleton);
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            return 1;
+        }
+        
+        
+        
+		
+		
+		
+		
+    }
+}
---
+++ Client/Assets/3rd/XLua/Src/LuaBehaviour.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System;
+
+[System.Serializable]
+public class Injection
+{
+    public string name;
+    public GameObject value;
+}
+
+[LuaCallCSharp]
+public class LuaBehaviour : MonoBehaviour
+{
+    public string luaScript;
+    public Injection[] injections;
+
+    internal static LuaEnv luaEnv = new LuaEnv(); //all lua behaviour shared one luaenv only!
+    internal static bool isAddLoader = false;
+    internal static float lastGCTime = 0;
+    internal const float GCInterval = 1;//1 second 
+
+    private Action luaStart;
+    private Action luaUpdate;
+    private Action luaOnDestroy;
+
+    private LuaTable scriptEnv;
+
+    public static LuaEnv GetEnv()
+    {
+        CheckAddLoader();
+
+        return luaEnv;
+    }
+
+    static void CheckAddLoader()
+    {
+        if (isAddLoader)
+            return;
+
+        luaEnv.AddBuildin("rapidjson", XLua.LuaDLL.Lua.LoadRapidJson);
+        luaEnv.AddBuildin("pb", XLua.LuaDLL.Lua.LoadPB);
+
+        luaEnv.AddLoader((ref string fileName) =>
+        {
+            return U3DUtility.ResManager.Singleton.GetLuaBytes(fileName);
+        });
+
+        isAddLoader = true;
+    }
+
+    void Awake()
+    {
+        CheckAddLoader();
+
+        scriptEnv = luaEnv.NewTable();
+
+        LuaTable meta = luaEnv.NewTable();
+        meta.Set("__index", luaEnv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();
+
+        scriptEnv.Set("self", this);
+        foreach (var injection in injections)
+        {
+            scriptEnv.Set(injection.name, injection.value);
+        }
+
+        if (!string.IsNullOrEmpty(luaScript))
+        {
+            byte[] buff = U3DUtility.ResManager.Singleton.GetLuaBytes(luaScript);
+            luaEnv.DoString(buff, "LuaBehaviour", scriptEnv);
+        }
+
+        Action luaAwake = scriptEnv.Get<Action>("awake");
+        scriptEnv.Get("start", out luaStart);
+        scriptEnv.Get("update", out luaUpdate);
+        scriptEnv.Get("ondestroy", out luaOnDestroy);
+
+        if (luaAwake != null)
+        {
+            luaAwake();
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (luaStart != null)
+        {
+            luaStart();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (luaUpdate != null)
+        {
+            luaUpdate();
+        }
+        if (Time.time - LuaBehaviour.lastGCTime > GCInterval)
+        {
+            luaEnv.Tick();
+            LuaBehaviour.lastGCTime = Time.time;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (luaOnDestroy != null)
+        {
+            luaOnDestroy();
+        }
+        luaOnDestroy = null;
+        luaUpdate = null;
+        luaStart = null;
+        scriptEnv.Dispose();
+        injections = null;
+    }
+}
